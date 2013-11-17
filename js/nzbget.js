ba04d9443b2d03200ad4ec9d870b99ee4dc03834
hps <hp@sarstrom.se>
2013-11-17 23:33:44 +0100

    Make sure to use same option class in api and options page. Add test button with result

--- js/nzbget.js
+++ js/nzbget.js
@@ -7,3 +7,62 @@ function main() {
 
     nzbGetChrome.loadMenu();
 }
+
+nzbGetOptions = new Class({
+	load: function() {
+		Array.each($$('input[type=text],input[type=password]'), function(o){
+			o.value = this.get(o.id);
+		},this);
+	}
+	,save: function() {
+		Array.each($$('input[type=text],input[type=password]'), function(o){
+			console.log('set',o.value);
+			localStorage[o.id] = o.value;
+		},this);
+	}
+	,get: function(opt) {
+		return typeof localStorage[opt] != 'undefined' ? localStorage[opt] : '';
+	}
+	,set: function(opt, value) {
+		localStorage[opt] = value;
+	}
+});
+
+nzbGetAPI = new Class({
+	initialize: function(){
+	}
+	,version: function() {
+		return this.sendMessage('version', {}, false);
+	}
+	,status: function() {
+		return this.sendMessage('status', {}, false);
+	}
+	,sendMessage: function(method, params, async) {
+        var url = this.Options.get('opt_host')
+        	,port = this.Options.get('opt_port')
+        	,username = this.Options.get('opt_username')
+        	,password = this.Options.get('opt_password')
+			,query = {
+	            version: '1.1'
+	            ,method: method
+	            ,params: params
+	        };
+
+        xhr = new Request.JSON({
+        	url: 'http://' + url + ":" + port + "/jsonrpc"
+        	,async: async
+        	,data: JSON.stringify(query)
+        	,user: username
+        	,password: password
+        }).send();
+        if(!async) {
+        	return JSON.parse(xhr.xhr.response);
+        }
+	}
+	,Options: new nzbGetOptions()
+});
+
+window.addEvent('domready', function(){
+	ngAPI = new nzbGetAPI();
+});
+
--- js/options.js
+++ js/options.js
@@ -1,47 +1,20 @@
 window.addEvent('domready', function(){
-	opts = new Options();
+	api = chrome.extension.getBackgroundPage().ngAPI;
+	opts = api.Options;
+
+	Array.each($$('input[type=text],input[type=password]'), function(o){
+		o.value = opts.get(o.id);
+	});
 	
 	$('btn_save').addEvent('click', function(){
-		opts.save();
+		Array.each($$('input[type=text],input[type=password]'), function(o){
+			opts.set(o.id, o.value);
+		});
 	});
-});
 
-Options = new Class({
-	initialize: function() {
-		this.load();
-	}
-	,load: function() {
-		Array.each($$('input[type=text],input[type=password]'), function(o){
-			o.value = this.get(o.id);
-		},this);
-	}
-	,save: function() {
-		Array.each($$('input[type=text],input[type=password]'), function(o){
-			console.log('set',o.value);
-			localStorage[o.id] = o.value;
-		},this);
-	}
-	,get: function(opt) {
-		return typeof localStorage[opt] != 'undefined' ? localStorage[opt] : '';
-	}
-	,testConnect: function() {
-        var url = this.get('opt_host')
-        	,port = this.get('opt_port')
-        	,username = this.get('opt_username')
-        	,password = this.get('opt_password');
+	$('btn_test').addEvent('click', function(){
+		var result = api.version();
+		new Element('div', {text: JSON.stringify(result)}).inject(document.body);
+	});
 
-        var query = {
-            version: '1.1'
-            ,method: "writelog"
-            //,method: 'version'
-            ,params: ["INFO", "Chrome extension successfully connected."]
-        };
-
-        new Request.JSON({
-        	url: 'http://' + url + ":" + port + "/jsonrpc"
-        	,data: JSON.encode(query)
-        	,user: username
-        	,password: password
-        }).send(query);
-	}
 });
