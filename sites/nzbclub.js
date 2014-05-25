e1905cf7422899d5583c7e4fd18a63d27a81853c
hps <hp@sarstrom.se>
2014-05-26 01:04:20 +0200

    Add one-click support for nzbclub.com. This closes #8.

---
+++ sites/nzbclub.js
@@ -0,0 +1,26 @@
+/**
+ * Inject nzbgc-markup to nzbclub.com
+ */
+
+dllinks = document.querySelectorAll('a[href*=\"/nzb_get/\"]');
+for(var i=0; i < dllinks.length; i++) { 
+	var dlitem = dllinks.item(i);
+	var category = '', lid = '';
+	
+	newdiv = document.createElement('span');
+	newdiv.className = 'icon_nzbget';
+	newdiv.style.padding = '0 10px';
+	var trParent = findParentOfType(dlitem, 'TR');
+	if(trParent && trParent.id) lid = trParent.id;
+
+	newitem = createNgIcon(
+		lid+'_nzbgc',
+		dlitem.href, 
+		category
+	);
+
+	newdiv.appendChild(newitem);
+
+	var dlparent = dlitem.parentElement;
+	dlparent.parentElement.insertBefore(newdiv,dlparent);
+}
