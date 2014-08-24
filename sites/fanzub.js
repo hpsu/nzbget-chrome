44953fa77028daf83cd6d413c78d552ce5a04a75
hps <hp@sarstrom.se>
2014-08-24 11:56:14 +0200

    Add support for fanzub.com. This fixes #11

---
+++ sites/fanzub.js
@@ -0,0 +1,26 @@
+/**
+ * Inject nzbgc-markup to fanzub.com
+ */
+
+dllinks = document.querySelectorAll('a[href*=\"/nzb/\"]');
+for(var i=0; i < dllinks.length; i++) {
+	var dlitem = dllinks.item(i);
+	var category = '', lid = '';
+
+	var trParent = findParentOfType(dlitem, 'TR');
+	if(trParent) {
+		var chb = trParent.querySelector('TD INPUT[type=checkbox]');
+		if(chb && chb.value) lid = chb.value;
+	}
+
+	newitem = createNgIcon(
+		lid+'_nzbgc',
+		dlitem.href,
+		category
+	);
+
+	newitem.style.verticalAlign='middle';
+	newitem.style.paddingRight='4px';
+	var dlparent = dlitem.parentElement;
+	dlparent.insertBefore(newitem,dlitem);
+}
